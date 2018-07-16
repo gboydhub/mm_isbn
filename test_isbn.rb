75eacc74863b3a1fec146417d81cf16e4528a957
Eponick <mikehuntwife@gmail.com>
2018-07-16 17:56:26 -0400

    test invalid isbn, fail

--- test_isbn.rb
+++ test_isbn.rb
@@ -5,4 +5,7 @@ class TestISBN < Minitest::Test
   def test_assert_1_equals_1
     assert_equal(1, 1)
   end
+  def test_invalid_isbn_is_false
+    assert_equal(false, valid_isbn?('abcd'))
+  end
 end

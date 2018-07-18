a5d3431f02f3db4420bf5f0b2cdb7160905effc0
Eponick <mikehuntwife@gmail.com>
2018-07-17 20:12:13 -0400

    test valid isbn10, pass

--- isbn.rb
+++ isbn.rb
@@ -1,3 +1,20 @@
 def valid_isbn?(isbn)
+  isbn_arr = isbn.split('')
+  isbn_sum = 0
+  isbn_checkdigit = isbn_arr.pop.to_i
+
+  isbn_arr.each_with_index do |digit, pos|
+    isbn_sum += digit.to_i * (pos+1)
+    #print "#{pos+1}x#{digit.to_i}, "
+  end
+  if isbn_sum == 0
+    return false
+  end
+
+  #print "\nSum: #{isbn_sum}, Rm: #{isbn_sum%11}, Dig: #{isbn_checkdigit}\n"
+  if isbn_sum % 11 == isbn_checkdigit
+    return true
+  end
+
   false
 end

fb432252b5fab9c4369da2018fef3db49be2852c
Eponick <mikehuntwife@gmail.com>
2018-07-17 20:33:39 -0400

    test valid isbn10 with x, pass

--- isbn.rb
+++ isbn.rb
@@ -1,7 +1,12 @@
 def valid_isbn?(isbn)
   isbn_arr = isbn.split('')
   isbn_sum = 0
-  isbn_checkdigit = isbn_arr.pop.to_i
+  isbn_checkdigit = isbn_arr.pop
+  if isbn_checkdigit.downcase == 'x'
+    isbn_checkdigit = 10
+  else
+    isbn_checkdigit = isbn_checkdigit.to_i
+  end
 
   isbn_arr.each_with_index do |digit, pos|
     isbn_sum += digit.to_i * (pos+1)
